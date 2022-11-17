bc4e0e582a0a7709672f24a631bf02a78c3cb9b7
Augustine Kim <augustinekim@google.com>
2022-11-17 15:05:21 -0800

    Add react wrapped component

--- packages/demos/nextjs-lit-ssr/pages/index.tsx
+++ packages/demos/nextjs-lit-ssr/pages/index.tsx
@@ -1,11 +1,14 @@
 import type {NextPage} from 'next';
 import '../src/simple-greeter.ts';
+// eslint-disable-next-line import/extensions
+import SimpleGreeter from '../src/SimpleGreeter';
 
 const Home: NextPage = () => {
   return (
     <div>
       <main>
-        <simple-greeter name="Friend"></simple-greeter>
+        {/* <simple-greeter name="Friend"></simple-greeter> */}
+        <SimpleGreeter name="React" />
       </main>
     </div>
   );
